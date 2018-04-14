d5875a494bdeeb2e018f7f4f90cb0849c94180ea
Andrew Jarrett <ahrjarrett@gmail.com>
2018-04-14 10:33:50 -0600

    finish answers; GET smurfs and pass as props to Smurfs component

---
+++ Answers.md
@@ -0,0 +1,23 @@
+1 .Explain the differences between client-side routing and server-side routing.
+
+Server-side routing lets the server handle what data is sent to the client based on what route the client requests. Client-side routing lets the JS code on the client dictate what data is rendered based on `window.location`.
+
+2. What does HTTP stand for?
+
+Hypertext Transfer Protocol.
+
+3. What does CRUD stand for?
+
+Create, Read, Update, Destroy (which maps to POST, GET, PUT, DELETE).
+
+4. Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
+
+Create  -> POST
+Read    -> GET
+Update  -> PUT
+Destroy -> DELETE
+
+5. Mention three tools we can use to make AJAX requests.
+
+The native XML API, jQuery, Axios.
+
--- village/src/App.js
+++ village/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
 class App extends Component {
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+  state = {
+    smurfs: []
+  }
+
+  componentDidMount = () => {
+    axios({
+      url: 'http://localhost:3333/smurfs',
+      method: 'GET',
+    }).then(response => this.setState(() => ({ smurfs: response.data })))
+  }
+
   render() {
     return (
       <div className="App">
         <SmurfForm />
-        <Smurfs />
+        <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
   }
